c1e520760af5cd482eb0bc7e5de7bd94d50601f4
Jay <jayliu925@gmail.com>
2020-10-01 22:32:55 -0600

    Use arrow functions

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,7 @@
 import '../styles/globals.css';
 import 'antd/dist/antd.css';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
+// eslint-disable-next-line react/prop-types
+const MyApp = ({ Component, pageProps }) => <Component {...pageProps} />;
 
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,41 +4,40 @@ import { Typography } from 'antd';
 
 const { Title } = Typography;
 
-export default function Home() {
-  return (
-    <div className={styles.app_container}>
-      <Head>
-        <title>Trello Clone</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+const Home = () => (
+  <div className={styles.app_container}>
+    <Head>
+      <title>Trello Clone</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
 
-      <main className={styles.main}>
-        <Title level={1}>
-          Clone of {' '}
-          <a
-            href="https://www.xiaohuiliu.me/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Trello
-          </a>
-        </Title>
-
-        <Title className={styles.description} level={4}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </Title>
-      </main>
-
-      <footer className={styles.footer}>
+    <main className={styles.main}>
+      <Title level={1}>
+        Clone of {' '}
         <a
           href="https://www.xiaohuiliu.me/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Developed by Xiaohui(Jay) Liu
+          Trello
         </a>
-      </footer>
-    </div>
-  );
-}
+      </Title>
+
+      <Title className={styles.description} level={4}>
+        Get started by editing{' '}
+        <code className={styles.code}>pages/index.js</code>
+      </Title>
+    </main>
+
+    <footer className={styles.footer}>
+      <a
+        href="https://www.xiaohuiliu.me/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Developed by Xiaohui(Jay) Liu
+      </a>
+    </footer>
+  </div>
+);
+export default Home;
