d07f81c289029478103c250b82307783cd72a201
xiaohui-j-liu <xiaohui.j.liu@gmail.com>
2020-12-14 22:56:02 -0700

    Task card stylings

--- components/task/task.tsx
+++ components/task/task.tsx
@@ -1,12 +1,14 @@
 import PropTypes from 'prop-types';
 import { Draggable } from 'react-beautiful-dnd';
 
-import { Typography, Card } from 'antd';
+import { Input } from 'antd';
 import styles from './task.module.scss';
 
-const { Title, Text } = Typography;
-
 const Task = ({ task, index }) => {
+  const onTaskTitleChange = (e) => {console.log(e)};
+
+  const onTaskBodyChange = (e) => {console.log(e)};
+
   return (
     <Draggable draggableId={task._id} index={index}>
       {(provided) => (
@@ -16,10 +18,36 @@ const Task = ({ task, index }) => {
           {...provided.draggableProps}
           {...provided.dragHandleProps}
         >
-          <Card>
-            <Title level={5}>{task.title}</Title>
-            <Text>{task.body}</Text>
-          </Card>
+          <div className={styles.task_card}>
+            <div className={styles.task_card_left}></div>
+            <div className={styles.task_card_right}>
+              <Input
+                bordered={false}
+                defaultValue={task.title}
+                maxLength={20}
+                onChange={onTaskTitleChange}
+                size={'middle'}
+                style={{
+                  backgroundColor: 'transparent',
+                  color: 'white',
+                  textAlign: 'center',
+                  fontWeight: 'bold',
+                }}
+              />
+              <Input
+                bordered={false}
+                defaultValue={task.body}
+                maxLength={50}
+                onChange={onTaskBodyChange}
+                size={'small'}
+                style={{
+                  backgroundColor: 'transparent',
+                  color: 'white',
+                  textAlign: 'center',
+                }}
+              />
+            </div>
+          </div>
         </div>
       )}
     </Draggable>
